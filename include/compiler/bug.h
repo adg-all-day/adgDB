3020b9a7b3cfcb763da4340012f08551a7baf16c
adg-all-day <tomadeaga@gmail.com>
2025-11-15 12:00:35 +0100

    compiler: add BUG() and assertion helpers

---
+++ include/compiler/bug.h
@@ -0,0 +1,44 @@
+#ifndef COMPILER_BUG_H
+#define COMPILER_BUG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <errno.h>
+#include "compiler/macro.h"
+
+static inline void die(const char *fmt, ...)
+{
+	va_list ap;
+	int ret = errno;
+
+	if (errno)
+		perror("adgdb");
+	else
+		ret = -1;
+
+	va_start(ap, fmt);
+	fprintf(stderr, "  ");
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+
+	fprintf(stderr, "\n");
+	exit(ret);
+}
+
+#define BUG_ON(cond)								\
+	do {									\
+		if (unlikely(cond))						\
+			die("BUG_ON: %s:%u:%s", __FILE__, __LINE__, __func__);	\
+	} while (0)
+
+#define BUG_ON_CUSTOM_MSG(cond, msg)								\
+	do {											\
+		if (unlikely(cond))								\
+			die("BUG_ON: %s:%u:%s :: %s", __FILE__, __LINE__, __func__, msg);	\
+	} while (0)
+
+
+#define BUG_GENERIC()	BUG_ON_CUSTOM_MSG(true, "Not implemented yet\n")
+
+#endif /* COMPILER_BUG_H */
