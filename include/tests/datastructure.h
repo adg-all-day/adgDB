311187aa758a2f8df66988938ddff8c3e8b855bc
adg-all-day <tomadeaga@gmail.com>
2025-12-21 11:00:01 +0100

    tests(ds): add datastructure test header and suite

---
+++ include/tests/datastructure.h
@@ -0,0 +1,50 @@
+#ifndef TESTS_DATASTRUCTURE_H
+#define TESTS_DATASTRUCTURE_H
+
+#include "tests/unittest.h"
+
+/* test suites */
+bool datastructure_init_suite(void);
+
+/* unit tests */
+void test_initialise_linkedlist(void);
+void test_linkedlist_add(void);
+void test_linkedlist_del(void);
+void test_linkedlist_iterate(void);
+
+void test_btree_init(void);
+void test_btree_destroy(void);
+void test_btree_lookup(void);
+void test_btree_insert__before_split(void);
+void test_btree_insert__root_split(void);
+void test_btree_insert__intermidiate_split(void);
+void test_btree_insert__increase_height(void);
+void test_btree_update(void);
+void test_btree_remove(void);
+
+void test_vector_init(void);
+void test_vector_push(void);
+void test_vector_free(void);
+
+void test_stack_init(void);
+void test_stack_push(void);
+void test_stack_pop(void);
+void test_stack_free(void);
+void test_stack_peek(void);
+void test_stack_peek_pos(void);
+
+void test_queue_init(void);
+void test_queue_offer(void);
+void test_queue_poll(void);
+void test_queue_free(void);
+void test_queue_peek(void);
+void test_queue_peek_pos(void);
+
+void test_hashtable_init(void);
+void test_hashtable_free(void);
+void test_hashtable_put(void);
+void test_hashtable_get(void);
+void test_hashtable_remove(void);
+void test_hashtable_iterate(void);
+
+#endif /* TESTS_DATASTRUCTURE_H */
---
+++ tests/datastructure/suite.c
@@ -0,0 +1,52 @@
+#include <tests/datastructure.h>
+
+bool datastructure_init_suite(void)
+{
+	CU_pSuite suite = NULL;
+
+	ADD_SUITE(suite, "datastructure");
+	/* linkedlist */
+	ADD_UNITTEST(suite, test_initialise_linkedlist);
+	ADD_UNITTEST(suite, test_linkedlist_add);
+	ADD_UNITTEST(suite, test_linkedlist_del);
+	ADD_UNITTEST(suite, test_linkedlist_iterate);
+	/* btree */
+	ADD_UNITTEST(suite, test_btree_init);
+	ADD_UNITTEST(suite, test_btree_destroy);
+	ADD_UNITTEST(suite, test_btree_lookup);
+	ADD_UNITTEST(suite, test_btree_insert__before_split);
+	ADD_UNITTEST(suite, test_btree_insert__root_split);
+	ADD_UNITTEST(suite, test_btree_insert__intermidiate_split);
+	ADD_UNITTEST(suite, test_btree_insert__increase_height);
+	ADD_UNITTEST(suite, test_btree_update);
+	ADD_UNITTEST(suite, test_btree_remove);
+	/* vector */
+	ADD_UNITTEST(suite, test_vector_init);
+	ADD_UNITTEST(suite, test_vector_push);
+	ADD_UNITTEST(suite, test_vector_free);
+	/* stack */
+	ADD_UNITTEST(suite, test_stack_init);
+	ADD_UNITTEST(suite, test_stack_push);
+	ADD_UNITTEST(suite, test_stack_pop);
+	ADD_UNITTEST(suite, test_stack_free);
+	ADD_UNITTEST(suite, test_stack_peek);
+	ADD_UNITTEST(suite, test_stack_peek_pos);
+	/* queue */
+	ADD_UNITTEST(suite, test_queue_init);
+	ADD_UNITTEST(suite, test_queue_offer);
+	ADD_UNITTEST(suite, test_queue_poll);
+	ADD_UNITTEST(suite, test_queue_free);
+	ADD_UNITTEST(suite, test_queue_peek);
+	ADD_UNITTEST(suite, test_queue_peek_pos);
+	
+	/* hashtable */
+	ADD_UNITTEST(suite, test_hashtable_init);
+	ADD_UNITTEST(suite, test_hashtable_free);
+	ADD_UNITTEST(suite, test_hashtable_put);
+	ADD_UNITTEST(suite, test_hashtable_get);
+	ADD_UNITTEST(suite, test_hashtable_remove);
+	ADD_UNITTEST(suite, test_hashtable_iterate);
+
+	return false;
+}
+
