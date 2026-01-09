3f7ecf6e294ea39f27f105768b348ac1cbf93e8f
adg-all-day <tomadeaga@gmail.com>
2026-01-09 09:47:52 +0100

    primitive: add table type

---
+++ include/primitive/table.h
@@ -0,0 +1,111 @@
+/*
+ * table.h
+ *
+ *  Created on: 4/03/2023
+ *      Author: paulo
+ */
+
+#ifndef INCLUDE_PRIMITIVE_TABLE_H_
+#define INCLUDE_PRIMITIVE_TABLE_H_
+
+#include <primitive/datablock.h>
+#include <primitive/column.h>
+#include <compiler/common.h>
+#include <lib/bit.h>
+
+#define TABLE_MAX_COLUMNS		128
+#define TABLE_MAX_NAME			127
+
+/* sanity checks */
+BUILD_BUG(TABLE_MAX_COLUMNS > 8, "TABLE_MAX_COLUMNS has to be greater than 8");
+BUILD_BUG(IS_POWER_OF_2(TABLE_MAX_COLUMNS), "TABLE_MAX_COLUMNS must be a power of 2");
+
+struct table {
+
+	char name[TABLE_MAX_NAME + 1 /*NUL char */];
+
+	struct column columns[TABLE_MAX_COLUMNS];
+	int column_count;
+
+	struct list_head *datablock_head;
+	/* offset from the last datablock item with free space available */
+	size_t free_dtbkl_offset;
+
+	/*
+	 * using mutex locks as it is POSIX.
+	 * I might, in the future, use futex (linux specific)
+	 * should performance needs a bit more attention.
+	 * Then again creating this database is more of a
+	 * curiosity exercise rather than anything else
+	 */
+	pthread_mutex_t mutex;
+};
+
+/**
+ * table_lock - lock a table
+ *
+ * @table: table reference
+ *
+ * Returns: 0 if successful, < 0 otherwise. See <error.h> for details.
+ */
+int __must_check table_lock(struct table *table);
+
+/**
+ * table_unlock - unlock a table
+ *
+ * @table: table reference
+ *
+ * Returns: 0 if successful, < 0 otherwise. See <error.h> for details.
+ */
+int table_unlock(struct table *table);
+
+/**
+ * table_init - initialise a table
+ *
+ * @name: name of the table
+ *
+ * This function returns struct table* or NULL if it fails to alloc memory
+ */
+struct table* __must_check table_init(char *name);
+
+/**
+ * table_destroy - free a table
+ *
+ * @table: table reference
+ *
+ * This function returns true if it could release all resources, false otherwise
+ * 
+ * Note: this method is not thread-safe. It is the caller's responsibility to
+ * call table_lock() before calling this method.
+ */
+bool table_destroy(struct table **table);
+
+/**
+ * table_vacuum - perform table vaccum on existing datablocks
+ *
+ * @table: table reference
+ * 
+ * Note: this method is not thread-safe. It is the caller's responsibility to
+ * call table_lock() before calling this method.
+ */
+bool table_vacuum(struct table *table);
+
+/**
+ * table_datablock_init - initialise datablock for a given table
+ *
+ * @table: table reference
+ * @offset: row offset
+ * @row_size: size of each row
+ */
+void table_datablock_init(struct datablock *block, size_t offset, size_t row_size);
+
+/**
+ * table_validate_name - valida name of table
+ *
+ * @name: name of the table
+ *
+ * this function returns true if name is valid, false otherwise
+ */
+bool table_validate_name(char *name);
+
+#endif /* INCLUDE_PRIMITIVE_TABLE_H_ */
---
+++ src/primitive/table.c
@@ -0,0 +1,134 @@
+/*
+ * table.c
+ *
+ *  Created on: 4/03/2023
+ *      Author: paulo
+ */
+
+#include <primitive/table.h>
+#include <primitive/column.h>
+#include <primitive/row.h>
+
+int table_lock(struct table *table)
+{
+	if (pthread_mutex_lock(&table->mutex))
+		return -ADGDB_INTERNAL;
+
+	return ADGDB_OK;
+}
+
+int table_unlock(struct table *table)
+{
+	if (pthread_mutex_unlock(&table->mutex))
+		return -ADGDB_INTERNAL;
+
+	return ADGDB_OK;
+}
+
+static inline bool __valid_name(char *name, size_t max_size)
+{
+	size_t arg_len;
+
+	arg_len = strlen(name);
+	if (arg_len == 0 || arg_len > max_size)
+		return false;
+
+	for (size_t i = 0; i < arg_len; i++) {
+		char c = name[i];
+		if (!((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||
+				(c >= '0' && c <= '9' && i != 0) ||
+				(c == '_' && i != 0)))
+			return false;
+	}
+	return true;
+}
+
+bool table_validate_name(char *name)
+{
+	return __valid_name(name, TABLE_MAX_NAME);
+}
+
+struct table* __must_check table_init(char *name)
+{
+	struct table *ret;
+
+	if (!name)
+		return NULL;
+
+	ret = zalloc(sizeof(*ret));
+	if (!ret)
+		return NULL;
+
+	ret->column_count = 0;
+	ret->free_dtbkl_offset = 0;
+
+	if (!table_validate_name(name))
+		goto err_free;
+
+	strncpy(ret->name, name, TABLE_MAX_NAME);
+
+	ret->datablock_head = datablock_init();
+	if (!ret->datablock_head)
+		goto err_free;
+
+	if (pthread_mutex_init(&ret->mutex, NULL))
+		goto err_datablock;
+
+	return ret;
+
+err_datablock:
+	free(ret->datablock_head);
+err_free:
+	free(ret);
+	return NULL;
+}
+
+static void __free_datablock_content(struct table *table, struct datablock *block)
+{
+	size_t row_size = table_calc_row_size(table);
+	for (size_t i = 0; i < (DATABLOCK_PAGE_SIZE / row_size); i++) {
+		struct row *row = (struct row*)&block->data[row_size * i];
+		table_free_row_content(table, row);
+	}
+}
+
+bool table_destroy(struct table **table)
+{
+	struct datablock *entry = NULL;
+	struct list_head *pos = NULL;
+	struct list_head *tmp_pos = NULL;
+
+	if (!(*table))
+		return false;
+
+	/* destroy table lock */
+	if (pthread_mutex_destroy(&(*table)->mutex))
+		return false;
+
+	/* destroy all datablocks if any */
+	list_for_each_safe(pos, tmp_pos, (*table)->datablock_head)
+	{
+		entry = list_entry(pos, typeof(*entry), head);
+
+		/* free variable precision value as they are alloc'ed separately */
+		__free_datablock_content(*table, entry);
+		datablock_free(entry);
+	}
+	free((*table)->datablock_head);
+
+	/* destroy table */
+	free(*table);
+	*table = NULL;
+
+	return true;
+}
+
+void table_datablock_init(struct datablock *block, size_t offset, size_t row_size)
+{
+	for (size_t i = offset / row_size; i < DATABLOCK_PAGE_SIZE / row_size; i++) {
+		struct row *row = (struct row*)&block->data[i * row_size];
+		row->flags.empty = true;
+		row->flags.deleted = false;
+		memzero((char* )row + sizeof(row->flags), row_size - offsetof(typeof(*row), null_bitmap));
+	}
+}
