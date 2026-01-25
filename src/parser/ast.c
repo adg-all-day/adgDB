a5156f34a670980f2a052825f93ba3b73993fb79
adg-all-day <tomadeaga@gmail.com>
2026-01-25 03:13:27 +0100

    parser: add AST core constructors

---
+++ src/parser/ast.c
@@ -0,0 +1,73 @@
+/*
+ * ast.c
+ *
+ *  Created on: 15/05/2023
+ *      Author: paulo
+ */
+
+#include <parser/ast.h>
+#include <lib/string.h>
+
+struct ast_node* ast_build_tree(struct queue *parser)
+{
+	char *str = NULL;
+	size_t pos = 0;
+	bool found = false;
+
+	/* sanity checks */
+	BUG_ON(!parser || queue_empty(parser));
+
+	/* walk to STMT */
+	while (pos < queue_length(parser)) {
+		if (strstarts((char*)queue_peek_pos(parser, pos), "STMT")) {
+			found = true;
+			break;
+		}
+		pos++;
+	}
+
+	/* something terribly wrong if this is true */
+	BUG_ON(!found);
+
+	/*
+	 * Notes to my future self:
+	 *
+	 * right now I'm just parsing a single statement but should I need to parse multiple
+	 * statements then this logic has to change
+	 */
+
+	/* decide which routine to best parse its contents based on the operation type */
+	pos--;
+	str = (char*)queue_peek_pos(parser, pos);
+	if (strstarts(str, "CREATE")) {
+		return ast_create_build_tree(parser);
+	} else if (strstarts(str, "INSERTVALS")) {
+		return ast_insert_build_tree(parser);
+	} else if (strstarts(str, "DELETEONE")) {
+		return ast_delete_build_tree(parser);
+	} else if (strstarts(str, "UPDATE")) {
+		return ast_update_build_tree(parser);
+	} else if (strstarts(str, "SELECT")) {
+		/* SELECT and SELECTNODATA */
+		return ast_select_build_tree(parser);
+	} else {
+		fprintf(stderr, "%s: %s handler not implement yet\n", __func__, str);
+		return NULL;
+	}
+}
+
+void ast_free(struct ast_node *node)
+{
+	struct list_head *pos;
+	struct list_head *tmp_pos;
+	struct ast_node *entry;
+
+	list_for_each_safe(pos,tmp_pos, node->node_children_head)
+	{
+		entry = list_entry(pos, typeof(*entry), head);
+		ast_free(entry);
+	}
+
+	free(node->node_children_head);
+	free(node);
+}
