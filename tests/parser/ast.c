058934a3eb3f1f70c1657279a4e9450b275c9a27
adg-all-day <tomadeaga@gmail.com>
2026-03-12 03:28:11 +0100

    tests(parser): cover AST core

---
+++ tests/parser/ast.c
@@ -0,0 +1,38 @@
+/*
+ * ast.c
+ *
+ *  Created on: 15/05/2023
+ *      Author: paulo
+ */
+
+#include <tests/parser.h>
+
+void test_ast_build_tree(void)
+{
+
+	/* CREATE tests */
+	test_ast_build_tree_create();
+
+	/* INSERT tests */
+	test_ast_build_tree_insert();
+
+	/* DELETE tests */
+	test_ast_build_tree_delete();
+
+	/* UPDATE tests */
+	test_ast_build_tree_update();
+
+	/* SELECT tests */
+	test_ast_build_tree_select();
+
+	/**
+	 *
+	 * Parser -> Syntax -> Semantic -> Optimiser -> Execution Plan (This works for Select, Insert, Update, Delete)
+	 * Parser -> Syntax -> Semantic -> Execution Plan (this works for CREATE, DROP?)
+	 *
+	 * Things to fix: 30/05/2023
+	 *
+	 * - tweak ast_build_tree so it can parse multiple STMTs in the future.
+	 */
+
+}
