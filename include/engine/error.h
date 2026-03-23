1138fa850f9d1cf9eb3cc916c9dc2c3dba91abfd
adg-all-day <tomadeaga@gmail.com>
2026-03-23 08:13:07 +0100

    engine: add error codes

---
+++ include/engine/error.h
@@ -0,0 +1,18 @@
+/*
+ * error.h
+ *
+ *  Created on: 21/02/2023
+ *      Author: paulo
+ */
+
+#ifndef INCLUDE_COMPILER_ERROR_H_
+#define INCLUDE_COMPILER_ERROR_H_
+
+#define	ADGDB_OK			0	/* Successful result */
+#define	ADGDB_ERROR			1	/* Generic error */
+#define	ADGDB_INTERNAL		2	/* Internal error - apps shouldn't see this */
+#define	ADGDB_NOMEM			3	/* Resource couldn't be allocated */
+#define	ADGDB_ROW			4	/* Next row is available */
+
+
+#endif /* INCLUDE_COMPILER_ERROR_H_ */
