989972dd8777c2941f70c09e1fa30ca8d44d4a12
adg-all-day <tomadeaga@gmail.com>
2026-04-23 19:48:33 +0100

    tests(engine): cover database context

---
+++ tests/engine/database.c
@@ -0,0 +1,76 @@
+/*
+ * adgdb.c
+ *
+ *  Created on: 9/06/2023
+ *      Author: paulo
+ */
+
+#include <tests/engine.h>
+#include <engine/database.h>
+
+void test_database_open(void)
+{
+	struct database db = {0};
+	CU_ASSERT_EQUAL(database_open(&db), ADGDB_OK);
+	CU_ASSERT_PTR_NOT_NULL(&db);
+	CU_ASSERT_PTR_NOT_NULL(db.tables);
+	database_close(&db);
+}
+
+void test_database_close(void)
+{
+	struct database db = {0};
+	CU_ASSERT_EQUAL(database_open(&db), ADGDB_OK);
+	database_close(&db);
+	CU_ASSERT_PTR_NULL(db.tables);
+}
+
+void test_database_add_table(void)
+{
+	struct database db = {0};
+	struct table *table_1;
+	struct table *table_2;
+	struct hashtable_value *entry;
+
+	CU_ASSERT_EQUAL(database_open(&db), ADGDB_OK);
+
+	/* insert table 1 */
+	table_1 = table_init("test_123");
+	CU_ASSERT_EQUAL(database_table_add(&db, table_1), ADGDB_OK);
+
+	/* check for table 1 */
+	entry = hashtable_get(db.tables, table_1->name, sizeof(table_1->name));
+	CU_ASSERT_PTR_NOT_NULL(entry);
+
+	/* insert table 2 */
+	table_2 = table_init("test_456");
+	CU_ASSERT_EQUAL(database_table_add(&db, table_2), ADGDB_OK);
+	/* check for table 1 && table 2*/
+	entry = hashtable_get(db.tables, table_1->name, sizeof(table_1->name));
+	CU_ASSERT_PTR_NOT_NULL(entry);
+	entry = hashtable_get(db.tables, table_2->name, sizeof(table_2->name));
+	CU_ASSERT_PTR_NOT_NULL(entry);
+
+	/* check for table that doesn't exist */
+	CU_ASSERT_PTR_NULL(hashtable_get(db.tables, "bogus", 6));
+
+	database_close(&db);
+
+}
+
+void test_database_table_exists(void)
+{
+	struct database db = {0};
+	struct table *table;
+
+	CU_ASSERT_EQUAL(database_open(&db), ADGDB_OK);
+
+	/* existing table  */
+	table = table_init("test_123");
+	CU_ASSERT_EQUAL(database_table_add(&db, table), ADGDB_OK);
+	CU_ASSERT(database_table_exists(&db, table->name));
+	/* non-existing table */
+	CU_ASSERT_FALSE(database_table_exists(&db, "bogus"));
+
+	database_close(&db);	
+}
