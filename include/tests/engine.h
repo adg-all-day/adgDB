ed754b5f91a37d640fad4fba0aba4e306e27fa1c
adg-all-day <tomadeaga@gmail.com>
2026-04-23 02:08:03 +0100

    tests(engine): add test header and suite

---
+++ include/tests/engine.h
@@ -0,0 +1,36 @@
+/*
+ * engine.h
+ *
+ *  Created on: 8/06/2023
+ *      Author: paulo
+ */
+
+#ifndef INCLUDE_TESTS_ENGINE_H_
+#define INCLUDE_TESTS_ENGINE_H_
+
+#include <tests/unittest.h>
+
+/* test suites */
+bool engine_init_suite(void);
+
+/* unit tests */
+void test_executor_run(void);
+
+void test_optimiser_run(void);
+
+void test_database_open(void);
+void test_database_close(void);
+void test_database_add_table(void);
+void test_database_table_exists(void);
+
+/* sub tests */
+void test_optimiser_insert(void);
+void test_optimiser_select(void);
+
+void test_executor_create(void);
+void test_executor_insert(void);
+void test_executor_delete(void);
+void test_executor_update(void);
+void test_executor_select(void);
+
+#endif /* INCLUDE_TESTS_ENGINE_H_ */
---
+++ tests/engine/suite.c
@@ -0,0 +1,23 @@
+#include "tests/engine.h"
+
+bool engine_init_suite(void)
+{
+	CU_pSuite suite = NULL;
+
+	ADD_SUITE(suite, "engine");
+
+	/* adgdb */
+	ADD_UNITTEST(suite, test_database_open);
+	ADD_UNITTEST(suite, test_database_close);
+	ADD_UNITTEST(suite, test_database_add_table);
+	ADD_UNITTEST(suite, test_database_table_exists);
+
+	/* executor */
+	ADD_UNITTEST(suite, test_optimiser_run);
+
+	/* executor */
+	ADD_UNITTEST(suite, test_executor_run);
+
+	return false;
+}
+
