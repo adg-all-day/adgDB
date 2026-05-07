8b7efd8cffb46be1ce1888c15232edae95f980be
adg-all-day <tomadeaga@gmail.com>
2026-05-07 22:57:54 +0100

    tests(engine): cover optimiser

---
+++ tests/engine/optimiser.c
@@ -0,0 +1,15 @@
+/*
+ * optimiser.c
+ *
+ *  Created on: 22/08/2023
+ *      Author: paulo
+ *
+ */
+
+#include <tests/engine.h>
+
+void test_optimiser_run(void)
+{
+	test_optimiser_insert();
+	test_optimiser_select();
+}
